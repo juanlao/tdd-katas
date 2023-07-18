e80cb1334d799e1ab5cd78fa45127178766f5a3b
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-18 19:46:51 +0200

    tdd: red

---
+++ passwordValidator/PasswordValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace passwordValidator
+{
+    internal class PasswordValidator
+    {
+        public PasswordValidator()
+        {
+        }
+
+        internal bool Validate(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ passwordValidator/Validator_Should.cs
@@ -0,0 +1,17 @@
+﻿namespace passwordValidator
+{
+    public class Validator_Should
+    {
+        [Fact]
+        public void MarkAsCorrectAPasswordWithMoreThan6Characters()
+        {
+            var validator = new PasswordValidator();
+
+            var isValid = validator.Validate("1234567");
+
+            isValid
+                .Should()
+                .BeTrue();
+        }
+    }
+}
