6dbd0a6c8773c3dffc99973bf336a084172816b1
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-18 19:58:28 +0200

    tdd: refactor

---
+++ passwordValidator/Password_Should.cs
@@ -0,0 +1,43 @@
+﻿namespace passwordValidator
+{
+    public class Password_Should
+    {
+        [Theory]
+        [InlineData("1234567", true)]
+        [InlineData("", false)]
+        public void HasMinimalLength(string password, bool expectedValidation)
+        {
+            var pass = new Password(password);
+
+            var isValid = pass.Validate(password);
+
+            isValid
+                .Should()
+                .Be(expectedValidation);
+        }
+
+        [Fact]
+        public void ValidateCapitalLetter()
+        {
+            Assert.Fail();
+        }
+
+        [Fact]
+        public void ValidateLowerCase()
+        {
+            Assert.Fail();
+        }
+
+        [Fact]
+        public void ContainANumber()
+        {
+            Assert.Fail();
+        }
+
+        [Fact]
+        public void ContainAnUnderscore()
+        {
+            Assert.Fail();
+        }
+    }
+}
