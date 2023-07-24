63ad1abe6b099fbff59b2cc82d22ab5f9b8a734c
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-24 19:47:30 +0200

    refactor: iva and igic are not present

--- CSVParser/CSVParser.cs
+++ CSVParser/CSVParser.cs
@@ -13,34 +13,43 @@
 
         internal List<string> filteredLines()
         {
-            if(this.lines == null)
+            if (this.lines == null)
             {
                 return new List<string>();
             }
 
-            var filteredLines = RemoveLinesWhereIVANeitherIGICArePresent(this.lines);
+            var filteredLines = RemoveInvalidLines(this.lines);
 
             return filteredLines;
         }
 
-        private List<string> RemoveLinesWhereIVANeitherIGICArePresent(List<string> lines)
+        private List<string> RemoveInvalidLines(List<string> lines)
         {
             var filteredLines = new List<string>();
-            foreach(var line in lines)
+            foreach (var line in lines)
             {
-                var fields = line.Split(',');
-                var iva = fields[4];
-                var igic = fields[5];
-                if (string.IsNullOrWhiteSpace(iva)
-                    && string.IsNullOrWhiteSpace(igic))
-                {
+                if (TaxFieldsAreNotPresent(line))
                     break;
-                }
 
                 filteredLines.Add(line);
             }
 
             return filteredLines;
+        }
+
+        private bool TaxFieldsAreNotPresent(string line)
+        {
+            var fields = line.Split(',');
+
+            var iva = fields[4];
+            var igic = fields[5];
+            if (string.IsNullOrWhiteSpace(iva)
+                && string.IsNullOrWhiteSpace(igic))
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
