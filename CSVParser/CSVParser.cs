0de2892d6c75393cf9fe6aeaf3f9c39ec8a7d017
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-24 19:40:53 +0200

    green: iva and igic are not present

--- CSVParser/CSVParser.cs
+++ CSVParser/CSVParser.cs
@@ -2,16 +2,45 @@
 {
     public class CSVParser
     {
-        private string content;
+        private readonly string content;
+        private readonly List<string> lines;
 
-        public CSVParser(string content)
+        public CSVParser(string content, List<string> lines)
         {
             this.content = content;
+            this.lines = lines;
         }
 
-        internal string Parse()
+        internal List<string> filteredLines()
         {
-            return content;
+            if(this.lines == null)
+            {
+                return new List<string>();
+            }
+
+            var filteredLines = RemoveLinesWhereIVANeitherIGICArePresent(this.lines);
+
+            return filteredLines;
+        }
+
+        private List<string> RemoveLinesWhereIVANeitherIGICArePresent(List<string> lines)
+        {
+            var filteredLines = new List<string>();
+            foreach(var line in lines)
+            {
+                var fields = line.Split(',');
+                var iva = fields[4];
+                var igic = fields[5];
+                if (string.IsNullOrWhiteSpace(iva)
+                    && string.IsNullOrWhiteSpace(igic))
+                {
+                    break;
+                }
+
+                filteredLines.Add(line);
+            }
+
+            return filteredLines;
         }
     }
 }
