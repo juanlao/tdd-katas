44368d82f516f73e729e4dade95e7cf18a0249af
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-25 19:31:18 +0200

    red: Two lines with same invoice number

--- CSVParser/CSVParser_Should.cs
+++ CSVParser/CSVParser_Should.cs
@@ -35,7 +35,7 @@ namespace CSVParser
         public void RemoveLineWhenTaxInformationIsNotDecimal()
         {
             var lineWithIVANotDecimal = "1,02/05/2019,1008,810,ASD,,ACERLaptop,B76430134,";
-            var lineWithIGICNotDecimal = "1,02/05/2019,1008,810,,ASD,ACERLaptop,B76430134,";
+            var lineWithIGICNotDecimal = "2,02/05/2019,1008,810,,ASD,ACERLaptop,B76430134,";
             var lines = new List<string> { lineWithIVANotDecimal, lineWithIGICNotDecimal };
 
             var parser = new CSVParser(header, lines);
@@ -45,11 +45,20 @@ namespace CSVParser
                 .BeEmpty();
         }
 
-        //[Fact]
-        //public void RemoveLinesWithSameInvoiceNumber()
-        //{
-        //    Assert.Fail("TODO");
-        //}
+        [Fact]
+        public void RemoveLinesWithSameInvoiceNumber()
+        {
+            var firstLine = "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,";
+            var secondLineWithSameInvoiceNumber = "1,02/05/2019,1008,810,19,,ACERLaptop,B76430134,";
+
+            var lines = new List<string> { firstLine, secondLineWithSameInvoiceNumber };
+
+            var parser = new CSVParser(header, lines);
+
+            parser.InvoiceLines()               
+                .Should()
+                .BeEmpty();
+        }
 
         //[Fact]
         //public void RemoveLinesWithCIFAndNIFSet()
