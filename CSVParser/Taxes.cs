aa6c92dcbb51c359d0067fbe6b4db6530bb9853f
Juan María Laó Ramos <juanlao@gmail.com>
2023-07-25 19:28:58 +0200

    green&Refactor: taxes are not decimal values

--- CSVParser/Taxes.cs
+++ CSVParser/Taxes.cs
@@ -1,28 +1,52 @@
-﻿namespace CSVParser
+﻿using System.Runtime.InteropServices;
+
+namespace CSVParser
 {
     internal class Taxes
     {
-        private string iva;
-        private string igic;
+        private readonly Tax iva;
+        private readonly Tax igic;
 
         public Taxes(string iva, string igic)
         {
-            this.iva = iva;
-            this.igic = igic;
+            this.iva = new Tax(iva);
+            this.igic = new Tax(igic);
         }
 
         internal bool IsValid()
         {
-            if (AreNotPresent(iva, igic))
-                return false;
+            if (ArePresent(iva, igic)
+                && AreValid(iva, igic))
+                return true;
 
-            return true;
+            return false;
         }
 
-        private bool AreNotPresent(string iva, string igic)
+        private bool AreValid(Tax iva, Tax igic)
         {
-            return string.IsNullOrWhiteSpace(iva)
-                && string.IsNullOrWhiteSpace(igic);
+            return iva.IsValid() || igic.IsValid();
+        }
+
+        private bool ArePresent(Tax iva, Tax igic)
+        {
+            return iva.Exist || igic.Exist;
+        }
+    }
+
+    internal class Tax
+    {
+        private readonly string tax;
+
+        public Tax(string tax)
+        {
+            this.tax = tax;
+        }
+
+        public bool Exist { get => !string.IsNullOrWhiteSpace(tax);}
+
+        internal bool IsValid()
+        {
+            return decimal.TryParse(tax, out decimal result);
         }
     }
 }
