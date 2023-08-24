131384d93dc60014c5165f249d151fba12f986d0
Juan María Laó Ramos <juanlao@gmail.com>
2023-08-24 20:35:39 +0200

    green: RemoveLinesWithCIFAndNIFSet

--- CSVParser/InvoiceLine.cs
+++ CSVParser/InvoiceLine.cs
@@ -10,11 +10,18 @@
             var iva = fields[4];
             var igic = fields[5];
 
+            
             Taxes = new Taxes(iva, igic);
+
+            cif = fields[7];
+            nif = fields[8];
+
             Number = fields[0];
         }
 
         public Taxes Taxes { get; private set; }
+
+        private string cif;
 
         public string Number { get; private set; }
 
